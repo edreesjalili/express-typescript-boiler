6efdfa7207c4e3deab6b500d9de01730d05c6d2d
Edrees Jalili <ejalili@localhost.localdomain>
2017-08-25 13:10:02 -0400

    updated auth middleware

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -3,3 +3,7 @@ import { Request, Response } from 'express';
 export function isLoggedIn() {
 	return (req: Request, res: Response, next: Function) => req.isAuthenticated() ? next() : res.redirect('/');
 }
+
+export function isAdmin() {
+	return (req: Request, res: Response, next: Function) => req.isAuthenticated() && req.user.group === 'admin' ? next() : res.redirect('/');
+}
