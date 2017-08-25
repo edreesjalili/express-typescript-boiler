8a6e41a6ac3deb8a65c0e043f47eed64e50cfe00
Edrees Jalili <ejalili@localhost.localdomain>
2017-08-25 11:03:07 -0400

    begin building out server

---
+++ src/config/redis.ts
@@ -0,0 +1,5 @@
+export default {
+	url: '127.0.0.1',
+	port: 6379,
+	secret: '',
+};
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,22 @@
-import { Router } from 'express';
+import * as express from 'express';
+import * as passport from 'passport';
+import * as compression from 'compression';
+import * as session from 'express-session';
+import * as redis from 'connect-redis';
+import * as config from './config/index';
 
+const app: express.Application = express();
+const redisStore: redis.RedisStore = redis(session);
+
+app.use(compression());
+app.use(session({
+	store: new redisStore({
+		url: config.redis.url,
+		port: config.redis.port,
+	}),
+	secret: '',
+	resave: false,
+	saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
